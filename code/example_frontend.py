4f902b4ae60244d5fc0c3d2c8d33a0845cf14bfc
zaid kamil <xaidmetamorphos@gmail.com>
2020-09-13 13:30:17 +0530

    datbase using sqlalchemy

---
+++ code/example_frontend.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import sessionmaker
+from project_orm import UserInput,Prediction
+from sqlalchemy import create_engine
+import streamlit as st
+
+engine = create_engine('sqlite:///project_db.sqlite3')
+Session = sessionmaker(bind=engine)
+sess = Session()
+
+st.title("Using database with SQLAlchemy")
+
+area = st.number_input('enter house area in sqft',
+                        max_value=10000,
+                        min_value=100,
+                        value = 400)
+
+rooms = st.number_input('enter no. of rooms',
+                        max_value=50,
+                        min_value=1,
+                        value = 3)
+
+age = st.number_input('age of house',
+                        max_value=500,
+                        min_value=0,
+                        value =1)
+location = st.text_area("enter location address")
+
+submit = st.button("make predictions")
+
+if submit and location:
+    try:
+        entry = UserInput(house_area=area,
+                      no_of_rooms=rooms,
+                      age = age,
+                      location = location)
+        sess.add(entry)
+        sess.commit()
+        st.success("data added to database")
+    except Exception as e:
+        st.error(f"some error occurred : {e}")
--- code/readme.md
+++ code/readme.md
@@ -3,3 +3,27 @@ we will store code in this folder
 - requirements.txt
 
 `contains all the libraries that are used in this project`
+
+to install the libraries
+
+```
+pip install -r path_to_requirements.txt
+```
+ex
+```
+pip install -r code code\requirements.txt
+```
+
+```
+pip install -r "C:\Users\xaidi\OneDrive\Documents\examples\example repo\code\requirements.txt"
+
+```
+to create the database 
+```
+python code\project_orm.py
+```
+
+to run the test file of streamlit
+```
+streamlit run code\example_frontend.py
+```
